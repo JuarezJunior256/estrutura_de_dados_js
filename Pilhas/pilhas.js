ffba6c721a709c409907c2c0c7d2e748b82fe1e4
JuarezJunior256 <juarezjunior.live@gmail.com>
2019-03-12 22:34:25 -0300

    Correção nos comentários dos metodos da classe Stack

--- Pilhas/pilhas.js
+++ Pilhas/pilhas.js
@@ -9,53 +9,56 @@ function Stack(){
    //EMPILHANDO ELEMENTOS NA PILHA
 
    this.push = function(element) {
-    items.push();
-   }
+    //add um novo item   
+    items.push(element);
+   };
 
    //DESEMPILHANDO ELEMENTOS DA PILHA
 
    this.pop = function() {
+       //remove item do topo da pilha
        return items.pop();
-   }
+   };
 
    //DEVOLVENDO ITEM NO TOPO DA PILHA
 
    this.peek = function() {
+       //devolve elemento que está no topo da pilha
        return items[items.length - 1];
-   }
+   };
 
    //VERIFICANDO SE A PILHA ESTÁ VAZIA
 
    this.isEmpty = function() {
+       //devolve true se a pilha não contiver elemento e false se for > que 0
        return items.length == 0;
-   }
+   };
 
    //TAMANHO DA PILHA
 
    this.size = function() {
+       // devolve o numero de elementos contidos na pilha
        return items.size();
-   }
+   };
 
    //LIMPANDO O ARRAY
 
    this.clear = function() {
+       //remove todos elementos da pilha
        items = [];
-   }
+   };
 
    //EXIBINDO ITEMS DA PILHA
 
    this.print = function() {
+       //mostrar itens da pilha
        console.log(items.toString());
        
-   } 
+   };
 }
 
+//USANDO A CLASSE STACK 
 
-push(element(s));// add um novo item
-pop();// remove item do topo da pilha
-peek();// devolve elemento que está no topo da pilha
-isEmpty()// devolve true se a pilha não contiver elemento e false se for > que 0
-clear();// remove todos elementos da pilha
-size();// devolve o numero de elementos contidos na pilha
-print();//mostrar itens da pilha
+let stack = new Stack();
 
+console.log(stack.isEmpty());
